a2cd8d2168db3e07597d75520d2f575cab12124b
K V Varun Krishnan <116856685+vrn21@users.noreply.github.com>
2023-10-10 16:27:32 +0530

    Poly Addition done

---
+++ PolynomialAddition.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct Polynomial{
+	int exp;
+	int coeff;
+};
+
+void polyCreate (int n, struct Polynomial poly[], int *start, int *finish){
+    for(int i = *start; i < n + *start; i++){
+        printf("Enter degree of term in the polynomial: ");
+        scanf("%d", &poly[i].exp);
+        printf("Enter coefficient for it: ");
+        scanf("%d", &poly[i].coeff);
+    }
+    *finish = *start + n;
+}
+
+void printPoly(struct Polynomial poly[], int start, int finish){
+	for(int i = start;i<finish;i++){
+		if (poly[i].coeff){
+		printf(" %dX%d ",poly[i].coeff,poly[i].exp);
+		}
+	}
+
+}
+
+void polyAdd(struct Polynomial poly[], int starta, int finisha, int startb, int finishb, int startc, int finishc){
+	//startc = finishb+1;
+	//finishc = startc-starta+finisha;
+	for(int i = starta;i<finisha;i++){
+		for(int j = startb;j<finishb;j++){
+			if (poly[i].exp == poly[j].exp){
+				int sum = poly[i].coeff + poly[j].coeff;
+				poly[j+i].exp = poly[i].exp;
+				poly[j+i].coeff = poly[i].coeff + poly[j].coeff;
+
+			}
+		}
+	}
+
+}
+
+
+void polyEval(struct Polynomial poly[], int start, int finish, int x ){
+	int val = 0;
+	int power,coef;
+	for(int i = start;i<finish;i++){
+		power = poly[i].exp;
+		coef = poly[i].coeff;
+		val += (coef*(pow(x,power)));
+
+	}
+	printf("THe final value is %d",val);
+}
+
+void main(){
+	struct Polynomial poly[100];
+	int n1, starta = 0 ,finisha = 0;
+
+	//polynomial1
+	printf("Enter details of your polynomial 1 \n");
+	printf("Enter number of terms in polynomial ");
+	scanf("%d",&n1);
+	polyCreate(n1,&poly,&starta,&finisha);
+	int x;
+	printf("ENter value of x ");
+	scanf("%d",&x);
+	polyEval(poly,starta,finisha,x);
+
+	//polynomial2
+
+	int n2,startb,finishb;
+	startb = finisha+1;
+	finishb = startb;
+	printf("Enter details of polynomial 2 \n ");
+	printf("Enter number of terms in polynmial ");
+	scanf("%d",&n2);
+	polyCreate(n2, poly,&startb,&finishb);
+
+	printf("The first polynomial is  \n");
+	printPoly(poly, starta,finisha);
+	printf("\n");
+	printf("The second polynomial is \n");
+	printPoly(poly,startb,finishb);
+	int startc,finishc;
+	startc = finishb+1;
+	finishc = startc + finisha - starta + finishb - startb;
+	polyAdd(&poly,starta,finisha,startb,finishb,startc,finishc);
+	printf("The added polynomial is \n");
+	printf("\n");
+	printPoly(poly,startc,finishc);
+
+}
+
+
